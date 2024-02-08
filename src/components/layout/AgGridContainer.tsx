1d7fcb5b5b9fe6c9026dda3b84cca1cbea06222f
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-02-08 17:37:48 +0545

    Feat: grid is populated with data from backend, along with loading and no data component.

--- src/components/layout/AgGridContainer.tsx
+++ src/components/layout/AgGridContainer.tsx
@@ -2,15 +2,36 @@ import { FC } from "react";
 import { AgGridReact } from "ag-grid-react"; // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import { CircularProgress, Flex, Text } from "@chakra-ui/react";
+
+const OnLoadingComponent = () => {
+  return (
+    <Flex gap={5}>
+      <CircularProgress isIndeterminate color="green.300" size="24px" />
+      <Text>Loading...</Text>
+    </Flex>
+  );
+};
 
 interface IAgGridContainer {
-  rowData: Array<any>;
   colDefs: Array<any>;
+  onGridReady: (params: any) => void;
 }
-const AgGridContainer: FC<IAgGridContainer> = ({ rowData, colDefs }) => {
+const AgGridContainer: FC<IAgGridContainer> = ({ colDefs, onGridReady }) => {
   return (
     <div className="ag-theme-quartz-dark" style={{ height: "800px" }}>
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      <AgGridReact
+        columnDefs={colDefs}
+        pagination={true}
+        rowModelType={"infinite"}
+        paginationPageSize={10}
+        paginationPageSizeSelector={[10, 20, 35, 50]}
+        onGridReady={onGridReady}
+        loadingOverlayComponent={OnLoadingComponent}
+        overlayNoRowsTemplate={
+          '<div class="ag-overlay-loading-center">No data available</div>'
+        }
+      />
     </div>
   );
 };
--- src/features/home/components/AgGridSection.tsx
+++ src/features/home/components/AgGridSection.tsx
@@ -1,32 +1,50 @@
 import { toast } from "react-toastify";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { colDefs } from "./colDefs";
 import AgGridContainer from "../../../components/layout/AgGridContainer";
 import { useFetchFilteredOrderWellLazyQuery } from "../../../generated/graphql";
 
 const AgGridSection = () => {
-  const [fetchOrderWell, { loading, error, data }] =
-    useFetchFilteredOrderWellLazyQuery();
-
+  const [gridApi, setGridApi] = useState<any>(null);
+  const [fetchOrderWell, { data }] = useFetchFilteredOrderWellLazyQuery();
   const columnDefs = useMemo(() => colDefs, []);
 
-  const rowData = useMemo(() => {
-    console.log("Loading", loading, "Error", error, "Data", data)
-    if (!loading && !error && data) {
-      return data.filteredOrderWell.collection;
-    } else {
-      if (error) {
-        toast.error(error.message);
-      }
-      return [];
-    }
-  }, [loading, error, data]);
+  const onGridReady = (params: any) => {
+    setGridApi(params.api);
+  };
 
   useEffect(() => {
     fetchOrderWell({ variables: { skip: 0, take: 10 } });
   }, [fetchOrderWell]);
 
-  return <AgGridContainer colDefs={columnDefs} rowData={rowData} />;
+  useEffect(() => {
+    if (gridApi) {
+      const dataSource = {
+        getRows: (params: any) => {
+          gridApi.showLoadingOverlay(); // Loading progress
+          fetchOrderWell({ variables: { skip: 0, take: 10 } })
+            .then((response) => response.data?.filteredOrderWell)
+            .then(({ collection, metadata }: any) => {
+              if (collection.length) {
+                gridApi.hideOverlay();
+              } else {
+                gridApi.showNoRowsOverlay();
+              }
+              params.successCallback(collection, metadata.totalCount);
+            })
+            .catch((error) => {
+              gridApi.showNoRowsOverlay();
+              toast.error(error.message);
+              params.successCallback([], 0);
+            });
+        },
+      };
+
+      gridApi.setDatasource(dataSource);
+    }
+  }, [gridApi, fetchOrderWell]);
+
+  return <AgGridContainer colDefs={columnDefs} onGridReady={onGridReady} />;
 };
 
 export default AgGridSection;
